3e9b9bed6c41849e78edd26a210fb7bf465b0e1f
James Lin <jameslin101@gmail.com>
2012-07-06 18:08:34 -0400

    query results in view

--- app/controllers/flight_searches_controller.rb
+++ app/controllers/flight_searches_controller.rb
@@ -24,8 +24,9 @@ class FlightSearchesController < ApplicationController
     from = @flight_search.departure_time.strftime("%Y-%m-%dT%H:%M")
     to = @flight_search.departure_time.end_of_day().strftime("%Y-%m-%dT%H:%M")
     #q = FlightStats.query({"Service"=>"SchedulesConnectionsService","from"=>from,"to"=>to,"origin.airportCode"=>origin_airportCode,"destination.airportCode"=>destination_airportCode,"flightNumber"=>flightNumber})
-    q =FlightStats.query({"Service"=>"SchedulesConnectionsService","from"=>"2012-07-04T12:00","to"=>"2012-07-04T12:15","origin.airportCode"=>"CLE","destination.airportCode"=>"PIT"})
-    @query_results = Process::process_query(q)
+    #q =FlightStats.query({"Service"=>"SchedulesConnectionsService","from"=>"2012-07-04T12:00","to"=>"2012-07-04T12:15","origin.airportCode"=>"CLE","destination.airportCode"=>"PIT"})
+    q = 
+    @query_results = Process::process_query(File.read("PIT.xml"))
   end
   
 
--- app/helpers/process.rb
+++ app/helpers/process.rb
@@ -1,26 +1,34 @@
 module Process
 
+  def Process.getVal(node,attrib)
+    if node.attribute(attrib)
+      node.attribute(attrib)
+    else
+      "WARNING DATA MISSING"
+    end
+  end
+
   def Process.process_query(pq)
     query_result = QueryResult.new
     qr = Nokogiri::XML(pq)
     qr.remove_namespaces!
     qr.xpath("//Flight").each do |f|
       flight = Flight.new
-      flight.arrival_date_adjustment = f.attribute("ArrivalDateAdjustment").value
-      flight.arrival_time = f.attribute("ArrivalTime").value
-      flight.departure_time = f.attribute("DepartureTime").value
-      flight.departure_date_to = f.attribute("DepartureDateTo").value
-      flight.departure_days_of_week = f.attribute("DepartureDaysOfWeek").value
-      flight.departure_time = f.attribute("DepartureTime").value
-      flight.distance_miles = f.attribute("DistanceMiles").value
-      flight.flight_duration_minutes = f.attribute("FlightDurationMinutes").value
-      flight.flight_type = f.attribute("FlightType").value
-      flight.layover_duration_minutes = f.attribute("LayoverDurationMinutes").value
-      flight.service_type = f.attribute("ServiceType").value
-      flight.departure_airport = Process.process_airport(f.xpath("DepartureAirport"))
-      flight.arrival_airport = Process.process_airport(f.xpath("ArrivalAirport"))
+      flight.arrival_date_adjustment = getVal(f,"ArrivalDateAdjustment")
+      flight.arrival_time = getVal(f,"ArrivalTime")
+      flight.departure_time = getVal(f,"DepartureTime")
+      flight.departure_date_to = getVal(f,"DepartureDateTo")
+      flight.departure_days_of_week = getVal(f,"DepartureDaysOfWeek")
+      flight.departure_time = getVal(f,"DepartureTime")
+      flight.distance_miles = getVal(f,"DistanceMiles")
+      flight.flight_duration_minutes = getVal(f,"FlightDurationMinutes")
+      flight.flight_type = getVal(f,"FlightType")
+      flight.layover_duration_minutes = getVal(f,"LayoverDurationMinutes")
+      flight.service_type = getVal(f,"ServiceType")
+      flight.departure_airport = process_airport(f.xpath("DepartureAirport"))
+      flight.arrival_airport = process_airport(f.xpath("ArrivalAirport"))
       f.xpath("FlightLeg").each do |l|
-        flight.flight_legs << Process::process_flight_leg(l)
+        flight.flight_legs << process_flight_leg(l)
       end
       query_result.flights << flight
     end
@@ -29,62 +37,61 @@ module Process
 
   def Process.process_airport(a)
     airport = Airport.new
-    airport.airport_code = a.attribute("AirportCode").value
-    airport.city = a.attribute("City").value
-    airport.country_code = a.attribute("CountryCode").value
-    airport.faa_code = a.attribute("FAACode").value
-    airport.iata_code = a.attribute("IATACode").value
-    airport.icao_code = a.attribute("ICAOCode").value
-    airport.name = a.attribute("Name").value
-    airport.state_code = a.attribute("StateCode").value
+    airport.airport_code = getVal(a,"AirportCode")
+    airport.city = getVal(a,"City")
+    airport.country_code = getVal(a,"CountryCode")
+    airport.faa_code = getVal(a,"FAACode")
+    airport.iata_code = getVal(a,"IATACode")
+    airport.icao_code = getVal(a,"ICAOCode")
+    airport.name = getVal(a,"Name").value
+    airport.state_code = getVal(a,"StateCode")
     airport
   end                  
+  
+
 
   def Process.process_flight_leg(l)
     leg = FlightLeg.new
-    leg.codeshare = l.attribute("Codeshare").value
-    leg.arrival_date_adjustment = l.attribute("ArrivalDateAdjustment").value
-    if l.attribute("ArrivalTerminal") then
-      leg.arrival_terminal = l.attribute("ArrivalTerminal").value
-    end
-    leg.arrival_time = l.attribute("ArrivalTime").value
-    leg.departure_date_adjustment = l.attribute("DepartureDateAdjustment").value
-    if l.attribute("ArrivalTerminal") then
-      leg.departure_terminal = l.attribute("DepartureTerminal").value
-    end
-    leg.departure_time = l.attribute("DepartureTime").value
-    leg.distance_miles = l.attribute("DistanceMiles").value
-    leg.flight_duration_minutes = l.attribute("FlightDurationMinutes").value
-    leg.layover_duration_minutes = l.attribute("LayoverDurationMinutes").value
-    leg.wetlease_info = l.attribute("WetleaseInfo").value
-    leg.flight_id_flight_number = l.xpath("FlightId").attribute("FlightNumber").value
-    leg.flight_id_carrier = Process.process_carrier(l.xpath("FlightId/Carrier"))
+    leg.codeshare = getVal(l,"Codeshare")
+    leg.arrival_date_adjustment = getVal(l,"ArrivalDateAdjustment")
+    leg.arrival_terminal = getVal(l,"ArrivalTerminal")
+    leg.arrival_time = getVal(l,"ArrivalTime")
+    leg.departure_date_adjustment = getVal(l,"DepartureDateAdjustment")
+    leg.departure_terminal = getVal(l,"DepartureTerminal")
+    leg.departure_time = getVal(l,"DepartureTime")
+    leg.distance_miles = getVal(l,"DistanceMiles")
+    leg.flight_duration_minutes = getVal(l,"FlightDurationMinutes")
+    leg.layover_duration_minutes = getVal(l,"LayoverDurationMinutes")
+    leg.wetlease_info = getVal(l,"WetleaseInfo")
+    leg.flight_id_flight_number = getVal(l.xpath("FlightId"),"FlightNumber")
+    leg.flight_id_carrier = process_carrier(l.xpath("FlightId/Carrier"))
     if l.xpath("Operator").length > 0
-      leg.operator_flight_number = l.xpath("FlightId").attribute("FlightNumber").value
-      leg.operator_carrier = Process.process_carrier(l.xpath("Operator/Carrier"))
+      leg.operator_flight_number = getVal(l.xpath("Operator"),"FlightNumber")
+      leg.operator_carrier = process_carrier(l.xpath("Operator/Carrier"))
     end
-    leg.departure_airport = Process.process_airport(l.xpath("DepartureAirport"))
-    leg.arrival_airport = Process.process_airport(l.xpath("ArrivalAirport"))
-    leg.equipment = Process.process_equipment(l.xpath("Equipment"))
+    leg.departure_airport = process_airport(l.xpath("DepartureAirport"))
+    leg.arrival_airport = process_airport(l.xpath("ArrivalAirport"))
+    leg.equipment = process_equipment(l.xpath("Equipment"))
+    leg
   end                  
 
   def Process.process_carrier(c)
     carrier = Carrier.new
-    carrier.airline_code = c.attribute("AirlineCode").value
-    carrier.iata_code = c.attribute("IATACode").value
-    carrier.icao_code = c.attribute("ICAOCode").value
-    carrier.name = c.attribute("Name").value
+    carrier.airline_code = getVal(c,"AirlineCode")
+    carrier.iata_code = getVal(c,"IATACode")
+    carrier.icao_code = getVal(c,"ICAOCode")
+    carrier.name = getVal(c,"Name")
     carrier
   end
 
   def Process.process_equipment(e)
     equipment = Equipment.new
-    equipment.aircraft_type_code = e.attribute("AircraftTypeCode").value
-    equipment.aircraft_type_name = e.attribute("AircraftTypeName").value
-    equipment.jet = e.attribute("Jet").value
-    equipment.regional = e.attribute("Regional").value
-    equipment.turboprop = e.attribute("Turboprop").value
-    equipment.wide_body = e.attribute("WideBody").value
+    equipment.aircraft_type_code = getVal(e,"AircraftTypeCode")
+    equipment.aircraft_type_name = getVal(e,"AircraftTypeName")
+    equipment.jet = getVal(e,"Jet")
+    equipment.regional = getVal(e,"Regional")
+    equipment.turboprop = getVal(e,"Turboprop")
+    equipment.wide_body = getVal(e,"WideBody")
     equipment
   end
 end
