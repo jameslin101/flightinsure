c94345a280e951d90ae6d7903dcd7b7293550ec8
James Lin <jameslin101@gmail.com>
2012-07-16 14:58:46 -0400

    checkboxes working

--- app/assets/javascripts/search_results.js
+++ app/assets/javascripts/search_results.js
@@ -29,11 +29,13 @@ jQuery(function() {
 
 		var total = 0;
 		$('.premium').each (function () {
-			var currency = $(this).val();
-			var number = Number(currency.replace(/[^0-9\.]+/g,""));
-			total += number;
+			if ($(this.parentNode.parentNode).children().find(".coverage_select").is(':checked')) {
+				var currency = $(this).val();
+				var number = Number(currency.replace(/[^0-9\.]+/g,""));
+				total += number;
+			}
 		});
-	    return "$" + total;
+	  return "$" + total;
 
 	}
 
@@ -41,44 +43,35 @@ jQuery(function() {
 
 		var total = 0;
 		$('.c4').each (function () {
-			var currency = $(this).data("val")
-			var number = Number(currency.replace(/[^0-9\.]+/g,""));
-			total += number;
+		  if ($(this.parentNode.parentNode).children().find(".coverage_select").is(':checked')) {			
+  			var currency = $(this).data("val")
+  			var number = Number(currency.replace(/[^0-9\.]+/g,""));
+  			total += number;
+			}
 		});
-	    return "$" + total;
+	  return "$" + total;
 
 	}
 	
 	
-
 	
 
-  $('.premium').on('change', function(event) {
-    //alert($(this).text());
-    //alert($(this).closest('td').next().children().attr('id'));
-	current_node = this;
-	
-	initPremium(this);
-	$('#total_premium').html(calcTotalPremium);
-	$('#total_coverage').html(calcTotalCoverage);
-	// p = $(current_node).children().filter(":selected").val();
-	// n = $.post("/getpayouts");
-	// 
-	// d = {"premium":p}
-	// $.getJSON('/getpayouts.json', d, function(data) {
-	//   var items = [];
-	// 
-	//   $.each(data, function(key, val) {
-	// 	trnode = current_node.parentNode.parentNode;
-	// 	$(trnode).children().find("#"+key).html(val)
-	// 
-	//   });
-	// 
-	// });
-	// //alert()
-    return event.preventDefault();
-  });
+	$('.premium').on('change', function(event) {
+		current_node = this;
 
+		initPremium(this);
+		$('#total_premium').html(calcTotalPremium);
+		$('#total_coverage').html(calcTotalCoverage);
+
+	   return event.preventDefault();
+	});
+
+	$('.coverage_select').on('change', function(event) {
+		$('#total_premium').html(calcTotalPremium);
+		$('#total_coverage').html(calcTotalCoverage);
+
+	   return event.preventDefault();
+	});
 
 	$('.premium').each(function(index) {
 	   initPremium(this);
