060188e0957fd2a7f1d8d14a59910744f2d73aa5
James Lin <jameslin101@gmail.com>
2012-07-27 14:57:20 -0400

    cleaned up landing page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
       render(association.to_s.singularize + "_fields", f: builder)
     end
-    link_to '<i class="icon-plus"></i>Add Flight'.html_safe, '#', class: "add_flight_search", data: {id: id, fields: fields.gsub("\n", "")}
+    link_to '<i class="icon-plus-sign"></i> Add Flight'.html_safe, '#', class: "add_flight_search", data: {id: id, fields: fields.gsub("\n", "")}
   end
   
 end
--- app/models/flight_search.rb
+++ app/models/flight_search.rb
@@ -7,10 +7,21 @@ class FlightSearch < ActiveRecord::Base
                   :origin,
                   :destination
   
-  # validates :flight_number, :format => { :with => /^[a-zA-Z]{2}\s?\d{2,4}$/, 
-  #                                        :message => "Invalid format"}
-  # validates :departure_date, :format => { :with => /^\d{2}\/\d{2}\/\d{4}$/,
-  #                                         :message => "Must be MM/DD/YYYY format"}
-  # 
+  validates :flight_number, :format => { :with => /^[a-zA-Z]{2}\s?\d{2,4}$/, 
+                                          :message => "Invalid format. Try airline code followed by flight number.", 
+                                          :allow_blank => true}
+                                          
+  validates :departure_date, :format => { :with => /^\d{2}\/\d{2}\/\d{4}$/,
+                                          :message => "Must be in MM/DD/YYYY format", 
+                                          :allow_blank => true}
+                                          
+  validates :origin, :format => { :with => /^[a-zA-Z]{3}$/,
+                                  :message => "Must enter airport code, which are 3 letters.", 
+                                  :allow_blank => true}
+                                  
+  validates :destination, :format => { :with => /^[a-zA-Z]{3}$/,
+                                       :message => "Must enter airport code, which are 3 letters.", 
+                                       :allow_blank => true}
+  
 end
 
