d026d74486d78b32f74cc0918ce308607589795c
James Lin <jameslin101@gmail.com>
2012-08-03 17:21:35 -0400

    paypal ipn test take 2 changed production mailer config

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,15 @@ require File.expand_path('../application', __FILE__)
 Front::Application.initialize!
 
 FLIGHTSTATS_GUID = "34b64945a69b9cac:-5b378c13:138494c0845:-45bd"
+
+config.action_mailer.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+  :address  => "smtp.gmail.com",
+  :port  => 587,
+  :user_name  => "james@stylefuze.com",
+  :password  => "1Mahustla",
+  :authentication  => :login
+}
+
+config.action_mailer.raise_delivery_errors = true
